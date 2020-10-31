7aacae0d83e9efcf972ef9cac61e57bc44d3bcca
Cuma Ali Efeyik <caefeyik@gmail.com>
2020-10-31 21:13:02 +0100

    LoginPage updated

--- src/test/java/com/library/pages/DashboardPage.java
+++ src/test/java/com/library/pages/DashboardPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.List;
 
 public class DashboardPage extends BasePage {
 
@@ -17,22 +16,5 @@ public class DashboardPage extends BasePage {
     @FindBy(xpath = "//img[@class='d-inline-block align-top']")
     public WebElement logo;
 
-    @FindBy(xpath = "//span[contains(text(),'Books')]")
-    public WebElement books;
-
-    @FindBy(xpath = "//span[contains(text(),'Borrowing Books')]")
-    public WebElement borrowingBooks;
-
-    @FindBy(xpath = "//a[contains(@onclick,'Books.borrow_book')]")
-    public WebElement borrowBookBtn;
-
-    @FindBy(xpath = "//div[@class='toast-message']")
-    public WebElement displayMessage;
-
-    @FindBy(xpath = "//i[contains(@class,'fa fa-angle-right')]")
-    public WebElement nextBtn;
-
-    @FindBy(linkText = "Return Book")
-    public List<WebElement> returnBookBtn;
 }
 
