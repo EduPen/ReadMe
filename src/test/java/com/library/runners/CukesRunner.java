e1534c019e1756057e7d5b2b0cda9a1c99ca5122
EduPen <zekeriya.bdtr@outlook.com>
2020-11-08 17:48:50 +0100

    .

--- src/test/java/com/library/runners/CukesRunner.java
+++ src/test/java/com/library/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features =  "src/test/resources/features",
         glue     =  "com/library/step_definitions",
         dryRun   = true,
-        tags     = "@L"
+        tags     = "@Dash"
 )
 
 public class CukesRunner{
