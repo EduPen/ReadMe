1598cc74ff8b7b089b0b99a2db176299e28b2d6a
EduPen <zekeriya.bdtr@outlook.com>
2020-11-09 19:08:28 +0100

    Dashboard Info verification  steps are ready

--- src/test/java/com/library/step_definitions/dashboardInformation_StepDef.java
+++ src/test/java/com/library/step_definitions/dashboardInformation_StepDef.java
@@ -1,4 +1,50 @@
 package com.library.step_definitions;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class dashboardInformation_StepDef {
+
+
+    @When("navigate to {string}")
+    public void navigate_to(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("calculate total borrowed books number")
+    public void calculate_total_borrowed_books_number() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("verify the barrowed books number are equal")
+    public void verify_the_barrowed_books_number_are_equal() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("calculate total books number")
+    public void calculate_total_books_number() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("verify the users  number are equal")
+    public void verify_the_users_number_are_equal() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("calculate total \\(active and inactive ) users")
+    public void calculate_total_active_and_inactive_users() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("verify the books numbers are equal")
+    public void verify_the_books_numbers_are_equal() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
