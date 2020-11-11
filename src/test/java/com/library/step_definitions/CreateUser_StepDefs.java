2a39b32a2191ad7fe3f86967b630449060f67bf2
Hilal Ay <hhilalayy1@gmail.com>
2020-11-11 09:38:24 +0100

    updated

--- src/test/java/com/library/step_definitions/CreateUser_StepDefs.java
+++ src/test/java/com/library/step_definitions/CreateUser_StepDefs.java
@@ -69,11 +69,6 @@ public class CreateUser_StepDefs {
     public void do_some_valid_changes(io.cucumber.datatable.DataTable dataTable) {
         // Write code here that turns the phrase above into concrete actions
         // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
         throw new io.cucumber.java.PendingException();
     }
 
