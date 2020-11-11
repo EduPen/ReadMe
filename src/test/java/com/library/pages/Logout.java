a8c89e6a1ba05e0bd938534d57d96142647db069
EduPen <zekeriya.bdtr@outlook.com>
2020-11-11 22:39:02 +0100

    fixed some codes

--- src/test/java/com/library/pages/Logout.java
+++ src/test/java/com/library/pages/Logout.java
@@ -1,10 +1,11 @@
 package com.library.pages;
 
+import com.library.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Logout extends BasePage {
-
 
 
 
