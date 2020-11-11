19b324560a4f9133f17e53e63c88074166d3d608
MehmetRasit <ulkermehmetrasit@gmail.com>
2020-11-11 22:41:33 +0100

    changed step definitions

--- src/test/java/com/library/step_definitions/SearchBox_StepDefs.java
+++ src/test/java/com/library/step_definitions/SearchBox_StepDefs.java
@@ -2,8 +2,31 @@ package com.library.step_definitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class SearchBox_StepDefs {
+
+    @Then("categories list should have following names:")
+    public void categories_list_should_have_following_names(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+
+    }
+
+    @When("search by {string}")
+    public void search_by(String string) {
+
+    }
+
+    @Then("verify the search with following column information")
+    public void verify_the_search_with_following_column_information() {
+
+    }
 
 
 
