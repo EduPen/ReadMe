e9448cc45425fd60a2d3a20333f531324890e5f0
MehmetRasit <ulkermehmetrasit@gmail.com>
2020-11-13 00:46:11 +0100

    changed step definitions

--- src/test/java/com/library/step_definitions/SearchBox_StepDefs.java
+++ src/test/java/com/library/step_definitions/SearchBox_StepDefs.java
@@ -1,31 +1,36 @@
 package com.library.step_definitions;
 
+import com.library.pages.BooksPage;
+import com.library.pages.UserPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.List;
 
 public class SearchBox_StepDefs {
 
     @Then("categories list should have following names:")
-    public void categories_list_should_have_following_names(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
+    public void categories_list_should_have_following_names(List<String> Categories) {
+        BooksPage booksPage = new BooksPage();
+        booksPage.BookCategories.click();
+
+
 
     }
 
     @When("search by {string}")
     public void search_by(String string) {
+        BooksPage booksPage = new BooksPage();
+        booksPage.Search.sendKeys("12345");
+        booksPage.Search.sendKeys("Selenium Book");
+        booksPage.Search.sendKeys("Jamal D");
 
     }
 
     @Then("verify the search with following column information")
     public void verify_the_search_with_following_column_information() {
-
     }
 
 
