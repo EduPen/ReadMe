71e94355fb6debbcc6e786b1846b395a5a8ac68f
Sergio4334 <SERCANAYTR3444@gmail.com>
2020-11-12 23:47:39 +0100

    Modules.feature and Modules_StepDefs.java are added also steps about module added

--- src/test/java/com/library/pages/DashboardPage.java
+++ src/test/java/com/library/pages/DashboardPage.java
@@ -2,8 +2,11 @@ package com.library.pages;
 
 import com.library.utilities.Driver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 
 public class DashboardPage extends BasePage {
@@ -34,6 +37,11 @@ public class DashboardPage extends BasePage {
 
     @FindBy(xpath = "Borrowed Books")
     public WebElement borrowedBooksText;
+
+    @FindBy(css = "h6.text-muted")
+    public List<WebElement> moduleNames;
+
+
 
 
 
